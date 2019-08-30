e570d77c73ee12081ff0d9a137ba27defbc39e2b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-30 11:50:22 -0600

    Add model spec tests for author and book that verify relationships and validations

---
+++ spec/models/books_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Book, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :number_of_pages}
+    it { should validate_presence_of :publication_year}
+  end
+
+  describe "relationships" do
+    it { should have_many :author_books}
+    it { should have_many(:authors).through(:author_books)}
+  end
+
+end
