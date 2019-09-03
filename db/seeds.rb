70e26db7002dc5ff558f6cd6eb09d10f80435d90
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-03 09:07:29 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.destroy_all
+Author.destroy_all
+
+george = Author.create!(name: "George RR Martin")
+mark = Author.create!(name: "Mark Lawrence")
+random = Author.create!(name: "Random Author Name")
+winds_of_winter = george.books.create!(title: "Winds of Winter", number_of_pages: 1300, publication_year: 2019)
+a_game_of_thrones = george.books.create!(title: "A Game of Thrones", number_of_pages: 1100, publication_year: 1999)
+prince_of_thorns = mark.books.create!(title: "Prince of Thorns", number_of_pages: 690, publication_year: 2013)
+random.books << prince_of_thorns
