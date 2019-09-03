022ced6e090bb339fc778328c0270ca03a2f6c7a
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-03 10:36:12 -0600

    Add model spec test for new author isntance method

--- spec/models/authors_spec.rb
+++ spec/models/authors_spec.rb
@@ -9,4 +9,13 @@ describe Author, type: :model do
     it {should have_many :author_books}
     it {should have_many(:books).through(:author_books)}
   end
+
+  describe "instance methods" do
+    it "can calculate average number of pages per book by the author" do
+    george = Author.create!(name: "George RR Martin")
+    winds_of_winter = george.books.create!(title: "Winds of Winter", number_of_pages: 1300, publication_year: 2019)
+    a_game_of_thrones = george.books.create!(title: "A Game of Thrones", number_of_pages: 1100, publication_year: 1999)
+    expect(george.average_number_of_pages).to eq(1200.0)
+    end
+  end
 end
