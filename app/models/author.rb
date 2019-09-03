00a7fd44c2acb07cc76a601fa9ae898a64b4dd3e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-03 09:08:45 -0600

    Add instance method average_number_of_pages to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,8 @@ class Author < ApplicationRecord
   has_many :author_books
   has_many :books, through: :author_books
   validates_presence_of :name
+
+  def average_number_of_pages
+    self.books.average(:number_of_pages)
+  end
 end
